318c354d9146b4a357eebe8500adf76542d7619e
Benfarhat <benfarhat.elyes@gmail.com>
2018-08-24 19:44:52 +0100

    add Assert/Constraints

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -18,19 +19,40 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email.",
+     *     checkMX = true
+     * )
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255)     * @Assert\Length(
+     *      min = 4,
+     *      minMessage = "Votre pseudonyme est trop court, veuillez en choisir un supérieur à {{ limit }} characteres"
+     * )
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 8,
+     *      minMessage = "Le mot de passe doit avoir au moins {{ limit }} characteres"
+     * )
+     * @Assert\EqualTo(
+     *     propertyPath = "confirm_password",
+     *     message = "Le mot de passe ne correspond pas à sa confirmation"
+     * )
      */
     private $password;
 
+    /**
+     * @Assert\EqualTo(
+     *     propertyPath = "confirm_password",
+     *     message = "Le mot de passe de confirmation ne correspond pas à celui précédemment fourni"
+     * )
+     */
     public $confirm_password;
 
     public function getId(): ?int
